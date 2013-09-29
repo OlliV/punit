309032d98eb31adaf9b4ec5850daad7d9cf19ff8
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-09-29 15:24:12 +0300

    Updated test project

--- examples/prj/test_testable.mk
+++ examples/prj/test_testable.mk
@@ -4,7 +4,7 @@
 TEST_SRC += test_testable.c
 
 # Infile src dir variable 
-sd-testable = ./testable
+sd-testable = ../testable
 
 # SRC files needed for this test
 SRC-testable += $(sd-testable)/testable.c
